7e98adfc358cb2357001410cff3b1becf98a4049
Ben Pomerenke <ben.pomerenke@asolutions.com>
2016-06-28 00:23:15 -0500

    001 - added uat - BP

--- TDDDemoApp.FunctionalTest/MessagesFixture.cs
+++ TDDDemoApp.FunctionalTest/MessagesFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -28,6 +29,35 @@ namespace TDDDemoApp.FunctionalTest
                 Assert.That(messages.Count, Is.EqualTo(2));
 
                 Thread.Sleep(2000);
+            }
+        }
+
+        [Test]
+        public void Can_Add_Comment()
+        {
+            DatabaseHelper.ExecuteCommand("DELETE FROM Messages");
+            DatabaseHelper.ExecuteCommand("INSERT INTO Messages (Message) VALUES('foo'),('Bar')");
+            using (var driver = new EventFiringWebDriver(new ChromeDriver()))
+            {
+                driver.Navigate().GoToUrl("http://localhost/TDDDemoApp/");
+
+                var loadingWait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+                loadingWait.Until(x => !x.FindElement(By.Id("message-loading")).Displayed);
+
+                const string inputMessage = "this is my message";
+                var inputElement = driver.FindElement(By.Id("message-text-input"));
+                inputElement.SendKeys(inputMessage);
+
+                var buttonElement = driver.FindElement(By.Id("add-message-button"));
+                buttonElement.Click();
+
+                var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+                wait.Until(x => string.IsNullOrEmpty(x.FindElement(By.Id("message-text-input")).Text));
+
+                Thread.Sleep(1000);
+                
+                var messages = driver.FindElements(By.TagName("blockquote"));
+                Assert.That(messages.Any(x=>x.Text.Contains(inputMessage)));
             }
         }
     }
